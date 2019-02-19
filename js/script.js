63a8af71c969f1b34f53a6ec16416c2d8d6d37f6
Dmitry Mekerov <dima.mekeroff@gmail.com>
2019-02-19 15:41:24 +0300

    slick slider styled

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-    $('.your-class').slick({
-      setting-name: setting-value
-    });
+$(document).ready(function () {
+  $('.intro-slider__slides').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
   });
+});
+
+$('.intro-slider__slides').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+  const $slider = $(this);
+  const $nextSlide = $($slider.find('.slick-slide')[nextSlide]);
+  const bgColor = $nextSlide.data('bg-color');
+  $('body').css('background-color', bgColor);
+});
+    
