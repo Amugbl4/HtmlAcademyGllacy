8a143081ce06a2d4d28a126d78cf0d6e7406434b
Dmitry Mekerov <dima.mekeroff@gmail.com>
2019-02-21 18:06:20 +0300

    js

--- js/script.js
+++ js/script.js
@@ -16,22 +16,45 @@ $('.intro-slider__slides').on('beforeChange', function (event, slick, currentSli
   $('body').css('background-color', bgColor);
 });
 
-// var headerSearch = document.querySelector('.user-block__list .search');
+const headerSearch = document.querySelector('.user-block__list .search');
 const $headerSearch = $('.user-block__list .search');
-// var popSearch = document.querySelector('.modal-search');
+const popSearch = document.querySelector('.modal-search');
 const $popSearch = $('.modal-search');
+
+/* $headerSearch.on('click', function (e) {
+  e.preventDefault();
+  $popSearch.toggleClass('modal-search--show');
+}) */
+
+// headerSearch.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   console.log(event);
+//   popSearch.classList.toggle("modal-search--show");
+//   setTimeout(function () {
+//     document.addEventListener("click", bodyClick);
+//   },50)
+// });
+
+// function bodyClick(event) {
+//     if ((event.target !== headerSearch) || (event.target !== popSearch)) {
+//       console.log(event.target);
+//       popSearch.classList.remove("modal-search--show");
+//   }
+//   document.removeEventListener("click", bodyClick)
+// }
 
 $headerSearch.on('click', function (e) {
   e.preventDefault();
-  $popSearch.toggleClass('modal-search--show');
+  $popSearch.addClass('modal-search--show');
+  console.log('1');
+  setTimeout(() => {
+    $(document).one('click', $headerSearch, function () {
+      console.log('2');
+      $popSearch.removeClass('modal-search--show');
+    })
+  }, 50);
 })
 
-// headerSearch.addEventListener("click", function (event) {
-//   console.log("клик по мышке")
-//   event.preventDefault();
-//   popSearch.classList.add("modal-search--show");
-// });
-    
 // headerSearch.addEventListener("click", function (event) {
 //   event.preventDefault();
 //   popSearch.classList.remove("modal-search--show");
