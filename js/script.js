d843280d41644a8eefbe0720aaa9b2bf6ca9b377
Dmitry Mekerov <dima.mekeroff@gmail.com>
2019-02-21 16:53:58 +0300

    finish slider & search popap

--- js/script.js
+++ js/script.js
@@ -15,4 +15,24 @@ $('.intro-slider__slides').on('beforeChange', function (event, slick, currentSli
   const bgColor = $nextSlide.data('bg-color');
   $('body').css('background-color', bgColor);
 });
+
+// var headerSearch = document.querySelector('.user-block__list .search');
+const $headerSearch = $('.user-block__list .search');
+// var popSearch = document.querySelector('.modal-search');
+const $popSearch = $('.modal-search');
+
+$headerSearch.on('click', function (e) {
+  e.preventDefault();
+  $popSearch.toggleClass('modal-search--show');
+})
+
+// headerSearch.addEventListener("click", function (event) {
+//   console.log("клик по мышке")
+//   event.preventDefault();
+//   popSearch.classList.add("modal-search--show");
+// });
     
+// headerSearch.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   popSearch.classList.remove("modal-search--show");
+// });
